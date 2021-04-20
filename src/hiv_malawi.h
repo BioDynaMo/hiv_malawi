3b523c80755d6a02d7334e10284a45112f69c2af
Tobias Duswald <tobias.duswald@cern.ch>
2021-04-20 10:44:28 +0200

    "Initial commit"

---
+++ src/hiv_malawi.h
@@ -0,0 +1,38 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2021 CERN & Newcastle University for the benefit of the
+// BioDynaMo collaboration. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// See the LICENSE file distributed with this work for details.
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.
+//
+// -----------------------------------------------------------------------------
+#ifndef HIV_MALAWI_H_
+#define HIV_MALAWI_H_
+
+#include "biodynamo.h"
+
+namespace bdm {
+
+inline int Simulate(int argc, const char** argv) {
+  Simulation simulation(argc, argv);
+
+  // Define initial model - in this example: single cell at origin
+  auto* rm = simulation.GetResourceManager();
+  auto* cell = new Cell(30);
+  rm->AddAgent(cell);
+
+  // Run simulation for one timestep
+  simulation.GetScheduler()->Simulate(1);
+
+  std::cout << "Simulation completed successfully!" << std::endl;
+  return 0;
+}
+
+}  // namespace bdm
+
+#endif  // HIV_MALAWI_H_

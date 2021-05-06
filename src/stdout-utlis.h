5a6280e7c4a2bf8bd5d9e3c6c530cdfc526d00ae
Tobias Duswald <tobias.duswald@cern.ch>
2021-05-06 22:22:06 +0200

    Add copyright notice

--- src/stdout-utlis.h
+++ src/stdout-utlis.h
@@ -1,3 +1,18 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2021 CERN (Tobias Duswald, Lukas Breitwieser, Ahmad Hesam, Fons
+// Rademakers) for the benefit of the BioDynaMo collaboration. All Rights 
+// Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// See the LICENSE file distributed with this work for details.
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.
+//
+// -----------------------------------------------------------------------------
+
 #ifndef STDOUT_UTILS_H_
 #define STDOUT_UTILS_H_
 
--- src/storage.cc
+++ src/storage.cc
@@ -1,3 +1,18 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2021 CERN (Tobias Duswald, Lukas Breitwieser, Ahmad Hesam, Fons
+// Rademakers) for the benefit of the BioDynaMo collaboration. All Rights 
+// Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// See the LICENSE file distributed with this work for details.
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.
+//
+// -----------------------------------------------------------------------------
+
 #include <iostream>
 #include <vector>
 #include "datatypes.h"

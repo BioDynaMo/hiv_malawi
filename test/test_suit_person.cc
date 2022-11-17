2c87839f9c5f8b0991238295d3aed4ad1ea63055
Tobias Duswald <tobias.duswald@cern.ch>
2022-11-17 12:55:31 +0100

    Add tests for person class

--- test/test_suit_person.cc
+++ test/test_suit_person.cc
@@ -11,11 +11,145 @@
 // -----------------------------------------------------------------------------
 
 #include <gtest/gtest.h>
+#include "person.h"
 
 #define TEST_NAME typeid(*this).name()
 
 namespace bdm {
+namespace hiv_malawi {
 
-TEST(PersonTest, TrivalTest) { EXPECT_DOUBLE_EQ(0, 0); }
+TEST(PersonTest, Age) {
+  Simulation simulation(TEST_NAME);
+  auto person = Person();
 
+  // Age
+  person.age_ = 1;
+  EXPECT_DOUBLE_EQ(person.age_, 1);
+  EXPECT_FALSE(person.IsAdult());
+  person.age_ = 16;
+  EXPECT_TRUE(person.IsAdult());
+}
+
+TEST(PersonTest, Sex) {
+  Simulation simulation(TEST_NAME);
+  auto person = Person();
+
+  // SEX
+  person.sex_ = Sex::kMale;
+  EXPECT_DOUBLE_EQ(person.sex_, Sex::kMale);
+  EXPECT_TRUE(person.IsMale());
+  EXPECT_FALSE(person.IsFemale());
+  person.sex_ = Sex::kFemale;
+  EXPECT_FALSE(person.IsMale());
+  EXPECT_TRUE(person.IsFemale());
+}
+
+TEST(PersonTest, Partner) {
+  Simulation simulation(TEST_NAME);
+  auto person = new Person();
+  auto partner = new Person();
+  auto* rm = simulation.GetResourceManager();
+  rm->AddAgent(person);
+  rm->AddAgent(partner);
+  auto ap_partner = partner->GetAgentPtr<Person>();
+
+  EXPECT_FALSE(person->hasPartner());
+  EXPECT_FALSE(partner->hasPartner());
+
+  // Set Partner
+  person->SetPartner(ap_partner);
+  EXPECT_TRUE(person->hasPartner());
+  EXPECT_TRUE(partner->hasPartner());
+
+  // Seperate Partner
+  person->SeparateFromPartner();
+  EXPECT_FALSE(person->hasPartner());
+  EXPECT_FALSE(partner->hasPartner());
+}
+
+TEST(PersonTest, MotherChild) {
+  Simulation simulation(TEST_NAME);
+  auto child = new Person();
+  auto mother = new Person();
+  auto* rm = simulation.GetResourceManager();
+  rm->AddAgent(child);
+  rm->AddAgent(mother);
+  auto ap_child = child->GetAgentPtr<Person>();
+  auto ap_mother = mother->GetAgentPtr<Person>();
+
+  EXPECT_EQ(mother->GetNumberOfChildren(), 0);
+  EXPECT_FALSE(mother->IsParentOf(ap_child));
+  EXPECT_FALSE(child->IsChildOf(ap_mother));
+
+  // Add child (does not add mother for child)
+  mother->AddChild(ap_child);
+  EXPECT_EQ(mother->GetNumberOfChildren(), 1);
+  EXPECT_TRUE(mother->IsParentOf(ap_child));
+  EXPECT_FALSE(child->IsChildOf(ap_mother));
+
+  // Add mother
+  child->mother_ = ap_mother;
+  EXPECT_EQ(mother->GetNumberOfChildren(), 1);
+  EXPECT_TRUE(mother->IsParentOf(ap_child));
+  EXPECT_TRUE(child->IsChildOf(ap_mother));
+
+  // Remove child (does not remove mother for child)
+  mother->RemoveChild(ap_child);
+  EXPECT_EQ(mother->GetNumberOfChildren(), 0);
+  EXPECT_FALSE(mother->IsParentOf(ap_child));
+  EXPECT_TRUE(child->IsChildOf(ap_mother));
+
+  // Remove mother
+  child->mother_ = nullptr;
+  EXPECT_EQ(mother->GetNumberOfChildren(), 0);
+  EXPECT_FALSE(mother->IsParentOf(ap_child));
+  EXPECT_FALSE(child->IsChildOf(ap_mother));
+}
+
+TEST(PersonTest, State) {
+  Simulation simulation(TEST_NAME);
+  auto person = Person();
+
+  // Healty
+  person.state_ = GemsState::kHealthy;
+  EXPECT_TRUE(person.IsHealthy());
+  EXPECT_FALSE(person.IsAcute());
+  EXPECT_FALSE(person.IsChronic());
+  EXPECT_FALSE(person.IsTreated());
+  EXPECT_FALSE(person.IsFailing());
+
+  // Acute
+  person.state_ = GemsState::kAcute;
+  EXPECT_FALSE(person.IsHealthy());
+  EXPECT_TRUE(person.IsAcute());
+  EXPECT_FALSE(person.IsChronic());
+  EXPECT_FALSE(person.IsTreated());
+  EXPECT_FALSE(person.IsFailing());
+
+  // Chronic
+  person.state_ = GemsState::kChronic;
+  EXPECT_FALSE(person.IsHealthy());
+  EXPECT_FALSE(person.IsAcute());
+  EXPECT_TRUE(person.IsChronic());
+  EXPECT_FALSE(person.IsTreated());
+  EXPECT_FALSE(person.IsFailing());
+
+  // Treated
+  person.state_ = GemsState::kTreated;
+  EXPECT_FALSE(person.IsHealthy());
+  EXPECT_FALSE(person.IsAcute());
+  EXPECT_FALSE(person.IsChronic());
+  EXPECT_TRUE(person.IsTreated());
+  EXPECT_FALSE(person.IsFailing());
+
+  // Failing
+  person.state_ = GemsState::kFailing;
+  EXPECT_FALSE(person.IsHealthy());
+  EXPECT_FALSE(person.IsAcute());
+  EXPECT_FALSE(person.IsChronic());
+  EXPECT_FALSE(person.IsTreated());
+  EXPECT_TRUE(person.IsFailing());
+}
+
+}  // namespace hiv_malawi
 }  // namespace bdm
